87bdeebbab082b0f036bfb0c091b63c31a1797ba
axel <axelperez221105@gmail.com>
2024-11-06 08:46:59 +0100

    falta por añadir button result

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("io.apisense:rhino-android:1.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
